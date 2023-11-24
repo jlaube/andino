ca2eaafb3ae03048296e17c5bbec204abee76f70
Juan A. Laube <laube@ekumenlabs.com>
2023-11-24 17:16:09 -0300

    RC1

--- andino_rqt/andino_rqt/calibration_widget.py
+++ andino_rqt/andino_rqt/calibration_widget.py
@@ -1,6 +1,8 @@
 import os
+import math
 
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 from python_qt_binding import loadUi
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtCore import Qt, pyqtSlot
@@ -19,22 +21,30 @@ class CalibrationWidget(QWidget):
         ui_file = os.path.join(package_path, 'share', 'andino_rqt', 'resource', 'andino_calibrate.ui')
         loadUi(ui_file, self)
         self.node = node
+        self.stop_when = 0.0
 
         # Config reader
         cfg_reader = AndinoConfigReader(node)
         self.wheel_radius_m.setText(cfg_reader.readWheelRadius())
+        self.speed = 0.0
 
         # Odometry reader
         self.odom_sub = self.node.create_subscription(Odometry, '/diff_controller/odom', self.odom_callback, 10)
 
+        # Andino comander
+        self.comander = self.node.create_publisher(Twist, '/diff_controller/cmd_vel_unstamped', 10)
+
         # Signals
         self.btnRun.pressed.connect(self.runCalibration)
-
         self.btnCalculate.pressed.connect(self.calculateCalibration)
+
+        self.timer = None
 
     @pyqtSlot()
     def runCalibration(self):
-        self.real_distance_m.setText("1.3")
+        self.stop_when = float(self.distance_m.text()) + float(self.odometry_x_m.text())
+        self.speed = 1.0
+        self.timer = self.node.create_timer(0.05, self.timer_callback)
 
     def calculateIdealRadius(self, distance, real_distance, wheel_radius=0.035):
         if distance == 0:
@@ -50,4 +60,20 @@ class CalibrationWidget(QWidget):
         self.odometry_x_m.setText(str(round(msg.pose.pose.position.x, 2)))
         self.odometry_y_m.setText(str(round(msg.pose.pose.position.y, 2)))
         self.odometry_z_m.setText(str(round(msg.pose.pose.position.z, 2)))
+
+    def timer_callback(self):
+        current_pos = float(self.odometry_x_m.text())
+        msg = Twist()
+
+        self.node.get_logger().info("current_pos: " + self.odometry_x_m.text() + " self.stop_when: " + str(self.stop_when))
+        if (abs(current_pos - self.stop_when) <= 0.03):
+            # Publish speed 0
+            self.speed = 0.0
+            self.timer.cancel()
+            self.timer.destroy()
+            self.node.get_logger().info("timer cancelled")
+
+
+        msg.linear.x = self.speed
+        self.comander.publish(msg)
 
